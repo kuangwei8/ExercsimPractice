15a435c2cf529ca152e483985925abc7ad683191
Wei Kuang <50929180+kuangwei8@users.noreply.github.com>
2019-10-25 21:59:49 -0700

    leap

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,6 +4,16 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        //      on every year that is evenly divisible by 4
+        //except every year that is evenly divisible by 100
+        //  unless the year is also evenly divisible by 400
+        bool leap = false; 
+        if((year % 4 == 0 && year % 100!=0) || (year % 100 ==0 && year %400 ==0))
+        {
+            leap = true; 
+        }
+        return leap; 
+             
+       
     }
 }
