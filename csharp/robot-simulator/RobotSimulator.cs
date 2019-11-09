81e02229d2064720a465fb23bcedf68676594c78
Wei Kuang <50929180+kuangwei8@users.noreply.github.com>
2019-11-09 10:05:20 -0800

    changes

---
+++ csharp/robot-simulator/RobotSimulator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
+
+public class RobotSimulator
+{
+    //constructor 
+    public RobotSimulator(Direction direction, int x, int y)
+    {
+        Direction = direction;
+        X = x;
+        Y = y; 
+    }
+    //properties 
+    public Direction Direction{ get;set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+   
+    // method 
+    public void Move(string instructions)
+    {
+        int tempDirection = (int)Direction;
+        foreach (char c in instructions)
+        {
+            if(c == 'R')
+            {
+                tempDirection += 1; 
+            }
+            if (c == 'L')
+            {
+                tempDirection -= 1; 
+            }
+        }
+
+
+        //if (tempDirection == 4)
+        //{
+        //    // north, x, y+1
+        //    tempDirection = 0;
+        //}
+
+        //if (tempDirection == -1)
+        //{
+        //    //east, x+1, y
+        //    tempDirection = 3;
+        //}
+    }
+}
